5f69d45348879ba0a82aafc574cbade2debe4c97
Valentin <val-glubokih@yandex.ru>
2023-07-08 18:24:55 +0300

    urls

---
+++ myapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import index, about, login, contact
+
+urlpatterns = [
+    path('', index),
+    path('about/', about),
+    path('login/', login),
+    path('contact/<int:id>', contact),
+]
